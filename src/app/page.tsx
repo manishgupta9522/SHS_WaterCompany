48f2998dd069199fc93877dbcf8515db870585ae
manishgupta9522 <maishgupta8772@gmail.com>
2024-09-19 00:58:23 +0530

    third commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/Components/Navbar/page";
 import AnimationVideo from "@/Components/AnimationVideo/page";
 import WhoWeAre from "@/Components/WhoWeAre/page";
+import "tailwindcss/tailwind.css";
 
 export default function Home() {
   return (
